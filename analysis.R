566482f775d78cde48929e98a92e484c266334bf
tranc10-1677612 <tranc10@uw.edu>
2022-03-04 01:21:20 -0800

    finished wb 5

--- analysis.R
+++ analysis.R
@@ -4,3 +4,31 @@
 library(foreign)
 library(survey)
 library(Hmisc)
+
+demo <- sasxport.get('DEMO_I.XPT')
+alco <- sasxport.get('ALQ_I.XPT')
+
+nhanes <- merge(x = demo, y = alco, by = 'seqn', all = TRUE)
+
+wt_sum <- sum(nhanes$wtint2yr, na.rm = TRUE)
+#this number represents the total US population at the time
+
+#Analysis
+
+#In ALQ151, we want 2 to be 0, and we want to ignore or disregard 7 and 9
+nhanes$alq151[nhanes$alq151 == 2] <- 0
+nhanes$alq151[nhanes$alq151 == 7] <- NA
+nhanes$alq151[nhanes$alq151 == 9] <- NA
+
+# create a survey design
+nhanes_survey <- svydesign(
+  id = ~sdmvpsu,
+  nest = TRUE,
+  strata = ~sdmvstra,
+  weights = ~wtint2yr,
+  data = nhanes
+)
+
+nhanes_mean <- svymean(~nhanes$alq151, nhanes_survey, na.rm = TRUE)
+
+mean_by_gender <- svyby(~alq151, ~riagendr, nhanes_survey, svymean, na.rm = TRUE)
